789e2cd612e140109121fcc85eb6a8d5e6c6e8de
Ugo Lancia <ugolancia@ugolancia.eu>
2015-07-10 12:00:09 +0200

    my final step to conclude course project 1

--- plot4.R
+++ plot4.R
@@ -15,12 +15,16 @@ ytime<- hms(y)
 date_time<- xtime+ytime
 powlyrsub<- cbind(date_time, powlyrsub)
 
-par(bg = "gray96")
+par(mfrow = c(2, 2), bg = "gray95")
+plot(powlyrsub$date_time, powlyrsub$Global_active_power, type = "l", xlab= "", ylab = "Global Active Power (kilowatts)")
+
+plot(powlyrsub$date_time, powlyrsub$Voltage, type = "l", xlab= "datetime", ylab = "Voltage")
+
 with(powlyrsub, plot(date_time, Sub_metering_1, type = "l", xlab= "", ylab = "Energy sub metering"))
 with(powlyrsub, lines(date_time, Sub_metering_2, col = "red"))
 with(powlyrsub, lines(date_time, Sub_metering_3, col = "blue"))
-legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1,1,1), col = c("black", "red", "blue"),  cex = 0.8)
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1,1,1), col = c("black", "red", "blue"), cex = 0.8)
+plot(powlyrsub$date_time, powlyrsub$Global_reactive_power, type = "l", xlab= "datetime", ylab = "Global_reactive_power")
 
-
-dev.copy(png, file = "plot3.png")
+dev.copy(png, file = "plot4.png")
 dev.off()
